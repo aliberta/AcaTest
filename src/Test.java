5966f3eb9259d43efaa0c40377a9b059f71b23f5
demirkapu <etudiant@192.168.10.177>
2015-02-27 21:35:36 +0100

    voila

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,23 @@
 
+
+
+
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Test a =new  Test();
+		a.affichage();
+			
+	}
+
+public void affichage()
+	{
+	
+		System.out.println("aLi t'es ou");
 
 	}
 
+	
 }
